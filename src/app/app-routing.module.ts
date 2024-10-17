b997eca67b36c175607c22bc01e366205959c28a
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2024-10-17 17:12:34 +0000

    client app starter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent,
+    children: [
+      { path: "", redirectTo: "", pathMatch: "full" },
+    ]
+
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SummaryComponent } from './components/dashboard/summary/summary.component';
+import { ProductOverviewComponent } from './components/dashboard/product-overview/product-overview.component';
+import { RecentSalesComponent } from './components/dashboard/recent-sales/recent-sales.component';
+import { UsersOnlineComponent } from './components/dashboard/users-online/users-online.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StocksComponent } from './components/dashboard/stocks/stocks.component';
+import { AccountProfileComponent } from './components/dashboard/account-profile/account-profile.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SummaryComponent,
+    ProductOverviewComponent,
+    RecentSalesComponent,
+    UsersOnlineComponent,
+    NavbarComponent,
+    StocksComponent,
+    AccountProfileComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
