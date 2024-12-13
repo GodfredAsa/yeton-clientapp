952aac8aada61790c5dd444632f8be997941f348
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2024-12-13 12:34:44 +0000

    first test

---
+++ src/app/components/model/item.model.ts
@@ -0,0 +1,14 @@
+export interface ItemModel {
+  categoryId: string;
+  name: string;
+  brand: string;
+  condition: string;
+  model: string;
+  price: number;
+  stock: number;
+  image: string;
+  hasGallery: boolean;
+  forSale: boolean;
+  hasVendor: boolean;
+  cost: number;
+}
